87ccdb4828d8b1e2b8a7d791b91dd5c1e9fbdff6
I-HarshyPaliwal <93369312+I-HarshyPaliwal@users.noreply.github.com>
2022-11-28 05:46:14 +0530

    Add files via upload

---
+++ javascript/card.js
@@ -0,0 +1,44 @@
+const card1 = document.querySelector("#card1");
+
+
+card1.addEventListener("click", function (e) {
+  card1.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector("#card2");
+
+
+card2.addEventListener("click", function (e) {
+  card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector("#card3");
+
+
+card3.addEventListener("click", function (e) {
+  card3.classList.toggle('is-flipped');
+});
+
+
+const card4 = document.querySelector("#card4");
+
+
+card4.addEventListener("click", function (e) {
+  card4.classList.toggle('is-flipped');
+});
+
+
+const card5 = document.querySelector("#card5");
+
+
+card5.addEventListener("click", function (e) {
+  card5.classList.toggle('is-flipped');
+});
+
+
+const card6 = document.querySelector("#card6");
+
+
+card6.addEventListener("click", function (e) {
+  card6.classList.toggle('is-flipped');
+});
